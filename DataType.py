99e133a94e97ee24b930ae6dc1989dd41ae06894
shruti bagul <160988588+shruti8767@users.noreply.github.com>
2025-08-15 12:39:00 +0530

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,25 @@
+##Integer example
+age=23
+print(type(age))
+
+##Floating point datatype
+height=5.11
+print(type(height))
+
+##String datatype example
+name="Shruti"
+print(type(name))
+
+##Boolean datatype
+is_true=True
+type(is_true)
+
+a=10
+b=10
+print(bool(a==b))
+print(type(a==b))
+
+##Datatype conversion int to str
+num=5
+result="Hello" + str(num)
+print(result)
