1f2e8035ea358adffecc88639dfa0ba0651c023d
shruti bagul <160988588+shruti8767@users.noreply.github.com>
2025-08-15 12:01:14 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+##Sample Calculator
+
+num1=float(input("Write a first number: "))
+num2=float(input("Write a second number: "))
+
+sum=num1+num2
+sub=num1-num2
+prod=num1*num2
+div=num1/num2
+rem=num1%num2
+
+print("Addition : ", sum)
+print("Substraction : ", sub)
+print("Product : ", prod)
+print("Division : ", div)
+print("Remainder : ", rem)
