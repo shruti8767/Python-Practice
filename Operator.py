6e08d0a0198e03319ee0218bd9f9ce290d518f75
shruti bagul <160988588+shruti8767@users.noreply.github.com>
2025-08-15 14:40:40 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,61 @@
+## Arithmatic operator
+
+a=10
+b=8
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+floor_div=a//b
+mod=a%b
+expo=a**b
+
+print("Addition: ",add)
+print("Substration: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+print("Floor_division: ",floor_div)
+print("Modulo: ",mod)
+print("Exponestial: ",expo)
+
+##Comparision Operator
+
+#Equal to ==
+str1="Shruti"
+str2="Shruti"
+print(str1==str2)
+
+#Not equal to !=
+print(str1!=str2)
+
+#greater than >
+num1=24
+num2=17
+print(num1>num2)
+
+#less than <
+print(num1<num2)
+
+#greater than or equal to >=
+print(num1>=num2)
+
+#less than or equal to <=
+print(num1<=num2)
+
+
+##Logical Operator 
+
+#AND- any one is false then gives false, otherwise true
+x=True
+y=True
+print("AND :",x and y) #True 
+
+#OR- any one is True then gives True , Otherwise False
+P=False
+Q=True
+print("OR :",P or Q)#True
+
+#NOT- only reverse
+s=True
+print("NOT :",not s)#False
