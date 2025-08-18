b1c9cf67b6804318c27802b490f38ab539f71bd9
shruti bagul <160988588+shruti8767@users.noreply.github.com>
2025-08-18 13:49:34 +0530

    Add files via upload

---
+++ Pass_Strength_Checker.py
@@ -0,0 +1,16 @@
+#Password Strength Checker
+
+def Pass_Check(password):
+    if len(password)<8:
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char.islower() for char in password):
+        return False
+    if not any(char.isupper() for char in password):
+        return False
+    if not any(char in '!@#$%^&*()_+' for char in password):
+        return False
+    return True
+
+print(Pass_Check("Shru@123"))
