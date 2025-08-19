034f239baf991dfdeabeff43a91998ddd46d0969
shruti bagul <160988588+shruti8767@users.noreply.github.com>
2025-08-19 12:21:20 +0530

    Add files via upload

---
+++ Package_pract.py
@@ -0,0 +1,9 @@
+from Packages.maths import addition
+print(addition(2,4))
+
+
+from Packages import maths
+print(maths.addition(5,6))
+
+from Packages.subpackages.mult import multiply
+print(multiply(5,9))
