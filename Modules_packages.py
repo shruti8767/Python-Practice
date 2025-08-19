4a4fbd21a9a685d58fb315bb7fac41e3147ef99b
shruti bagul <160988588+shruti8767@users.noreply.github.com>
2025-08-19 12:04:35 +0530

    Add files via upload

---
+++ Modules_packages.py
@@ -0,0 +1,12 @@
+#Importing modules in python : Modules and Packages
+
+import math
+print(math.sqrt(16))
+
+from math import sqrt, pi
+print(sqrt(25))
+print(pi)
+
+import numpy as np 
+arr=np.array([1,2,3,4])
+print(arr)
